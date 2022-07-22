5faf99bb82da9d10342b9f53ce2be7006d0cfbc1
Enis <87227909+izmir10@users.noreply.github.com>
2022-07-22 22:02:56 +0200

    Pages added

--- src/test/java/com/cydeo/pages/LoginPage.java
+++ src/test/java/com/cydeo/pages/LoginPage.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
+
 public class LoginPage {
     public LoginPage(){
         PageFactory.initElements(Driver.getDriver(), this);
@@ -13,13 +15,26 @@ public class LoginPage {
     @FindBy(id="prependedInput")
     public WebElement userName;
 
-
-
     @FindBy(id="prependedInput2")
     public WebElement password;
 
     @FindBy(name = "_submit")
     public WebElement submit;
+
+    @FindBy (css = ".btn.btn-primary")
+    public List<WebElement> buttons;
+
+    @FindBy (xpath = "//div[@class='alert alert-error']")
+    public WebElement alertMessage;
+
+    @FindBy (linkText = "Forgot your password?")
+    public WebElement forgotPasswordLink;
+
+    @FindBy (xpath = "//span[@class='custom-checkbox__icon']")
+    public WebElement rememberCheckBox;
+
+    @FindBy (xpath = "//span[@class='custom-checkbox__text']")
+    public WebElement textOfRememberCheckBox;
 
 
     public void login(String userNameStr, String passwordStr) {
